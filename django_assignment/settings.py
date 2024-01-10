c4ec9d409185663d350070fd071fc048747fc275
Miro Zukina <92750661+MiroZukina@users.noreply.github.com>
2024-01-10 16:35:08 +0000

    Add files via upload

--- django_assignment/settings.py
+++ django_assignment/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "Felse").lower() == "true"
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
