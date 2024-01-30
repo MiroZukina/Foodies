72fef1113b0e58ae786f85b3232b2287185d369d
Miro Zukina <92750661+MiroZukina@users.noreply.github.com>
2024-01-30 23:03:56 +0000

    Add files via upload

--- django_assignment/settings.py
+++ django_assignment/settings.py
@@ -24,16 +24,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY ='django-insecure-_mx_z%wr0r20q=gi^upogu_=s#okwigi+a=g)z8+!yoooy+gw%'
+#SECRET_KEY ='django-insecure-_mx_z%wr0r20q=gi^upogu_=s#okwigi+a=g)z8+!yoooy+gw%'
+
+SECRET_KEY =os.environ("SECRET_KEY ")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-#DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+#DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS  = []
+#ALLOWED_HOSTS  = []
 
-#ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
-
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('RENDER_HOSTNAME', '')]
 
 
 # Application definition
@@ -83,16 +85,18 @@ WSGI_APPLICATION = 'django_assignment.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}'''
+}
+database_url = os.environ.get("DATABESE_URL")
 
-#database_url = os.environ.get("DATABESE_URL")
+DATABASES["defaul"] = dj_database_url(database_url)
 
+''''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -102,7 +106,7 @@ DATABASES = {
         'HOST': 'dpg-cme5e2mn7f5s739f50og-a',
         'PORT': '5432',
     }
-}
+}'''
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
